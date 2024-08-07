83d3325f4cb3cec5e437763de3afbc6ddfbbb2b2
Guxxtavoww <gustavoaugustocar@hotmail.com>
2024-08-07 15:14:03 -0300

    first commit

--- activation-functions.py
+++ activation-functions.py
@@ -1,7 +1,16 @@
 import numpy as np
 
-def stepFunction(soma: float):
-    if soma >= 1:
+def stepFunction(sum: float):
+    if sum >= 1:
         return 1;
     return 0;
 
+def sigmoidFunction(sum: float) -> float:
+    return 1 / (1 + np.exp(-sum))
+
+def hyperbolicTangentFunction(x: float):
+    return (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))
+
+user_input = float(input('Insira um x para a função hyper: '))
+
+print(f"hyper: {hyperbolicTangentFunction(user_input)}")
