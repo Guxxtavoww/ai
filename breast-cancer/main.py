f7591578900ac373f8750fdef7e2984d6af8c431
Gustavo Augusto <81970233+Guxxtavoww@users.noreply.github.com>
2024-08-12 16:32:15 -0300

    Chore: first commit

--- breast-cancer/main.py
+++ breast-cancer/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from keras import Sequential, layers
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score
 
 classe = pd.read_csv('./saidas_breast.csv');
 previsores = pd.read_csv('./entradas_breast.csv');
@@ -12,3 +13,13 @@ classfyer.add(layers.Dense(units=16, activation='relu', kernel_initializer='rand
 classfyer.add(layers.Dense(units=1, activation='sigmoid'));
 classfyer.compile(optimizer='adam', loss='binary_crossentropy', metrics=['binary_accuracy']);
 classfyer.fit(previsores_treinamento, classe_treinamento, batch_size=10, epochs=100)
+
+previsoes = classfyer.predict(previsores_teste)
+previsoes = previsoes >= 0.5
+
+precision = accuracy_score(classe_teste, previsoes)
+matriz = confusion_matrix(classe_teste, previsoes)
+
+result = classfyer.evaluate(previsores_teste, classe_teste)
+
+print(result)
